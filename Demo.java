38a73208a69e48a94163d36a5f669342c430c809
Manishkumarsamal-258 <samalmanishkumar5@gmail.com>
2026-04-24 12:47:43 +0530

    update the code

---
+++ Demo.java
@@ -0,0 +1,20 @@
+// check palindrome number
+public class Demo {
+    public static void main(String[] args) {
+        int num = 12321;
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10; // Get the last digit
+            reversedNum = reversedNum * 10 + digit; // Append the digit to the reversed number
+            num /= 10; // Remove the last digit
+        }
+
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome number.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome number.");
+        }
+    }
+}
